6fdfbae7f174a5c25d1557ce2d92025ee1f3bca0
Alberto Duarte <betodute@gmail.com>
2023-08-30 19:40:40 -0700

    All model / table, associations and validations complete
    Sessions and Tweets

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,2 +1,5 @@
 class Tweet < ApplicationRecord
+  belongs_to :user
+  validates :user_id, presence: true
+  validates :message, presence: true, length: {minimum: 0, maximum: 140}
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_08_31_022130) do
+ActiveRecord::Schema.define(version: 2023_08_31_023701) do
+
+  create_table "sessions", force: :cascade do |t|
+    t.string "token"
+    t.integer "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "tweets", force: :cascade do |t|
+    t.string "message"
+    t.integer "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
